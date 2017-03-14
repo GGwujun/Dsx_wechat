2369ef14fc0b905fefd3588d09c43a19669febde
GGwujun <876337269@qq.com>
2017-03-14 17:43:24 +0800

    到当地

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import 'babel-polyfill';
 import Vue from 'vue';
 import App from './App';
 import store from './store';
+import router from './router';
 
 Vue.config.devtools = true;
 
 new Vue({
     el: '#app',
+	router,
     template: '<App/>',
     components: { App },
     store:store
